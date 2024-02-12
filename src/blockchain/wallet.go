6af51c300081413c5d7d3347215252141055d7d3
sdant4786 <s.dant4786@gmail.com>
2024-02-11 19:14:11 -0500

    Fixed saving and loading of wallets

--- src/blockchain/wallet.go
+++ src/blockchain/wallet.go
@@ -4,16 +4,17 @@ import (
 	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"crypto/x509"
 	"encoding/gob"
+	"encoding/pem"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 )
 
 const (
 	version            = byte(0x00)
-	walletFile         = "wallet.dat"
+	walletFile         = "wallet_%s.dat"
 	addressChecksumLen = 4
 )
 
@@ -61,25 +62,36 @@ func (ws Wallets) GetWallet(address string) Wallet {
 	return *ws.Wallets[address]
 }
 
+type walletIntermediary struct {
+	PrivateKey []byte
+	PublicKey  []byte
+}
+
 func (ws *Wallets) LoadFromFile(nodeID string) error {
 	walletFile := fmt.Sprintf(walletFile, nodeID)
 	if _, err := os.Stat(walletFile); os.IsNotExist(err) {
 		return err
 	}
 
-	fileContent, err := ioutil.ReadFile(walletFile)
+	fileContent, err := os.ReadFile(walletFile)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	var wallets Wallets
+	walletsIntermediary := map[string]walletIntermediary{}
 	gob.Register(elliptic.P256())
 	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
-	err = decoder.Decode(&wallets)
+	err = decoder.Decode(&walletsIntermediary)
 	if err != nil {
 		log.Panic(err)
 	}
+	wallets := Wallets{Wallets: map[string]*Wallet{}}
 
+	for name, w := range walletsIntermediary {
+		privateKey := decode(w.PrivateKey)
+		w := Wallet{PrivateKey: *privateKey, PublicKey: w.PublicKey}
+		wallets.Wallets[name] = &w
+	}
 	ws.Wallets = wallets.Wallets
 
 	return nil
@@ -87,18 +99,41 @@ func (ws *Wallets) LoadFromFile(nodeID string) error {
 
 func (ws Wallets) SaveToFile(nodeID string) {
 	var content bytes.Buffer
+	walletsIntermediary := map[string]walletIntermediary{}
 	walletFile := fmt.Sprintf(walletFile, nodeID)
 
+	for name, wallet := range ws.Wallets {
+		privateKey := encode(&wallet.PrivateKey)
+		walletsIntermediary[name] = walletIntermediary{
+			PrivateKey: privateKey,
+			PublicKey:  wallet.PublicKey,
+		}
+	}
 	gob.Register(elliptic.P256())
 
 	encoder := gob.NewEncoder(&content)
-	err := encoder.Encode(ws)
+	err := encoder.Encode(walletsIntermediary)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	err = ioutil.WriteFile(walletFile, content.Bytes(), 0644)
+	err = os.WriteFile(walletFile, content.Bytes(), 0644)
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func encode(privateKey *ecdsa.PrivateKey) []byte {
+	x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
+	pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: x509Encoded})
+
+	return pemEncoded
+}
+
+func decode(pemEncoded []byte) *ecdsa.PrivateKey {
+	block, _ := pem.Decode(pemEncoded)
+	x509Encoded := block.Bytes
+	privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
+
+	return privateKey
 }
